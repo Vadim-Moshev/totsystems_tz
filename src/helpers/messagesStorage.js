00570c71834c22139b4f7ef70d511104e674882f
Vadim Moshev <reg-vadim-moshev@yandex.ru>
2021-02-23 02:35:56 +0500

    Added message by other user to imitate corporative communication

--- src/helpers/messagesStorage.js
+++ src/helpers/messagesStorage.js
@@ -1,3 +1,5 @@
+import { messagesByOtherUsers } from "./messagesByOtherUsers";
+
 const isCorporativeMessage = (stringifiedValue, working) => {
   let message;
   try {
@@ -12,6 +14,22 @@ const isCorporativeMessage = (stringifiedValue, working) => {
 const byUnixDate = (a, b) => a.id - b.id;
 
 class MessagesStorage {
+  constructor() {
+    // Это просто инициализация локалСторадж для имитации корпоративного общения
+    const keys = Object.keys(localStorage).map(Number);
+    const flag = keys.some((key) => {
+      return [1, 2, 3, 4, 5, 6, 7].includes(key);
+    });
+
+    if (flag) {
+      return;
+    }
+
+    messagesByOtherUsers.forEach((msg, index) => {
+      this.addMessage(index + 1, msg);
+    });
+  }
+
   addMessage = (id, message) => {
     localStorage.setItem(id, JSON.stringify(message));
   };
